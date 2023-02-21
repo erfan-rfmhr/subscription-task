15e3abb691217aeddf958b4a28faeb1d641cbf6a
erfan <erfan.arefmehr@gmail.com>
2023-02-22 01:39:30 +0330

    initialize db_admin

--- src/database/db.py
+++ src/database/db.py
@@ -1,15 +1,11 @@
+from fastapi_amis_admin.admin.settings import Settings
+from fastapi_amis_admin.admin.site import AdminSite
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+
+db_admin = AdminSite(settings=Settings(database_url_async='sqlite+aiosqlite:///database.db'))
+
 engine = create_engine('sqlite:///database.db', connect_args={'check_same_thread': False})
-
 Base = declarative_base()
-session = sessionmaker(bind=engine)
 
-
-def get_db():
-    db = session()
-    try:
-        yield db
-    finally:
-        db.close()
