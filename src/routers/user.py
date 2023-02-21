e80272a51732a545a0fea1118e13a32d1fdc98f1
erfan <erfan.arefmehr@gmail.com>
2023-02-22 00:19:23 +0330

    signup endpoint: get request

---
+++ src/routers/user.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+router = APIRouter(prefix="/user", tags=["user"])
+user_templates = Jinja2Templates(directory="templates/user")
+
+
+@router.get("/signup", response_class=HTMLResponse)
+async def signup(request: Request):
+    return user_templates.TemplateResponse("signup.html", {"request": request})
