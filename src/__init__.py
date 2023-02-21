cf8fcfefef95f87a13c233c4f98cb920a2a81868
erfan <erfan.arefmehr@gmail.com>
2023-02-22 00:20:08 +0330

    include user router

--- src/__init__.py
+++ src/__init__.py
@@ -3,13 +3,15 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+from routers import user
 from src.routers import user
 
 app = FastAPI()
+app.include_router(user.router)
 app.mount("/statics", StaticFiles(directory="statics"), name="statics")
-templates = Jinja2Templates(directory="templates")
+jinja_templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
 async def home_page(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    return jinja_templates.TemplateResponse("home.html", {"request": request})
