38965e3dd9151c3a32460ce4bfa2cf13e27d7110
erfan <erfan.arefmehr@gmail.com>
2023-02-22 01:38:26 +0330

    create signup post method

--- src/routers/user.py
+++ src/routers/user.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from src.database.models import Customer
+from src.database.db import db_admin
 
 router = APIRouter(prefix="/user", tags=["user"])
 user_templates = Jinja2Templates(directory="templates/user")
@@ -9,3 +11,10 @@ user_templates = Jinja2Templates(directory="templates/user")
 @router.get("/signup", response_class=HTMLResponse)
 async def signup(request: Request):
     return user_templates.TemplateResponse("signup.html", {"request": request})
+
+
+@router.post("/signup")
+async def signup(request: Request, username: str = Form(...), email: str = Form(...), password: str = Form(...)):
+    user = Customer(username=username, email=email, password=password)
+    db_admin.db.add(user)
+    return {"username": username, "email": email, "password": password}
