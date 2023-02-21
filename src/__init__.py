ca32c83de0c970ca58c54d5adaff83e549c8ab71
erfan <erfan.arefmehr@gmail.com>
2023-02-22 01:37:36 +0330

    add db_admin to app

--- src/__init__.py
+++ src/__init__.py
@@ -4,12 +4,16 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 from routers import user
-from src.routers import user
+from src.database.db import db_admin, Base, engine
 
 app = FastAPI()
+
 app.include_router(user.router)
+
 app.mount("/statics", StaticFiles(directory="statics"), name="statics")
 jinja_templates = Jinja2Templates(directory="templates")
+db_admin.mount_app(app)
+Base.metadata.create_all(bind=engine)
 
 
 @app.get("/", response_class=HTMLResponse)
