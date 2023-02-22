8e3d8b7f0a4b98da02c8266ad17eeacc9fb5239a
erfan <erfan.arefmehr@gmail.com>
2023-02-22 10:56:43 +0330

    create login user loader, fix async functions in add_user

--- src/database/db_user.py
+++ src/database/db_user.py
@@ -1,10 +1,17 @@
-from src.database.db import db_admin
-from src.database.models import Customer
+from database.db import db_admin
+from database.models import Customer
+from authentication.auth import login_manager
 
 
-def add_user(username, email, password):
+async def add_user(username, email, password):
     user = Customer(username=username, email=email, password=password)
     db_admin.db.add(user)
-    db_admin.db.commit()
-    db_admin.db.refresh(user)
-    return user
+    await db_admin.db.commit()
+    await db_admin.db.refresh(user)
+
+
+@login_manager.user_loader()
+async def get_user_by_username(username):
+    cursor = await db_admin.db.async_execute(statement="SELECT * FROM customers WHERE username = :username",
+                                             params={"username": username})
+    return cursor.fetchone()
