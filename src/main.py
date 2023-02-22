e9436a7e83fd1cce9ac697712a1c11999f43be8e
erfan <erfan.arefmehr@gmail.com>
2023-02-22 10:57:39 +0330

    move reqiurmets into main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,25 @@
 import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
-from src import app
+from database.db import Base, db_admin, engine
+from routers import user
+
+app = FastAPI()
+
+app.include_router(user.router)
+
+app.mount("/statics", StaticFiles(directory="statics"), name="statics")
+jinja_templates = Jinja2Templates(directory="templates")
+db_admin.mount_app(app)
+Base.metadata.create_all(bind=engine)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home_page(request: Request):
+    return jinja_templates.TemplateResponse("home.html", {"request": request})
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
