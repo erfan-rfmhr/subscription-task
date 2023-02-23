6c974ba104688ace02ef1259b087a17a48c6f743
erfan <erfan.arefmehr@gmail.com>
2023-02-24 00:11:26 +0330

    change product to subscription

--- src/database/db_subscription.py
+++ src/database/db_subscription.py
@@ -5,9 +5,9 @@ from .models import Subscription
 async def create_subscriptions():
     cursor = await db_admin.db.execute("SELECT * FROM subscriptions")
     if len(cursor.fetchall()) == 0:
-        sub1 = Subscription(name="Product 1", price=10)
-        sub2 = Subscription(name="Product 2", price=20)
-        sub3 = Subscription(name="Product 3", price=30)
+        sub1 = Subscription(name="Subscription 1", price=10)
+        sub2 = Subscription(name="Subscription 2", price=20)
+        sub3 = Subscription(name="Subscription 3", price=30)
         db_admin.db.add_all([sub1, sub2, sub3])
         await db_admin.db.commit()
         await db_admin.db.refresh(sub1)
