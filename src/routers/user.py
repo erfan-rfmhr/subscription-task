12ac9c2cbf114513d1acd850faa3fbf429e60a8b
erfan <erfan.arefmehr@gmail.com>
2023-02-23 10:32:21 +0330

    login post method: if user does exist, create access token else redirect

--- src/routers/user.py
+++ src/routers/user.py
@@ -1,5 +1,5 @@
 from authentication.auth import login_manager
-from database.db_user import add_user
+from database.db_user import add_user, get_user
 from fastapi import APIRouter, Request, Form, status, Depends
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
@@ -27,6 +27,21 @@ async def signup(request: Request, username: str = Form(...), email: str = Form(
 @router.get("/login", response_class=HTMLResponse)
 async def login(request: Request):
     return user_templates.TemplateResponse("login.html", {"request": request})
+
+
+@router.post("/login")
+async def login(request: Request, username: str = Form(...), password: str = Form(...)):
+    user = await get_user(username)
+    if not user:
+        return RedirectResponse(url="/user/login", status_code=status.HTTP_302_FOUND)
+    if not (password == user.password and username == user.username):
+        return RedirectResponse(url="/user/login", status_code=status.HTTP_302_FOUND)
+    access_token = login_manager.create_access_token(
+        data={"sub": username}
+    )
+    response = RedirectResponse(url="/user/account", status_code=status.HTTP_302_FOUND)
+    login_manager.set_cookie(response=response, token=access_token)
+    return response
 
 
 @router.get("/account", response_class=HTMLResponse)
