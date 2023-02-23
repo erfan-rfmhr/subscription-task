267de31d794f5b582a16a9f11885a0147b63c873
erfan <erfan.arefmehr@gmail.com>
2023-02-23 18:53:01 +0330

    run scheduler task on startup

--- src/main.py
+++ src/main.py
@@ -2,13 +2,15 @@ import asyncio
 
 import uvicorn
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.middleware.cors import CORSMiddleware
+
 from database.db import Base, db_admin, engine
 from database.db_subscription import create_subscriptions
 from routers import user
+from task_managment.task_scheduler import scheduler
 
 app = FastAPI()
 
@@ -29,6 +31,18 @@ db_admin.mount_app(app)
 Base.metadata.create_all(bind=engine)
 # create subscriptions if they don't exist
 asyncio.run(create_subscriptions())
+
+
+@app.on_event("startup")
+async def start_task():
+    scheduler.start()
+
+
+@app.on_event("shutdown")
+async def shutdown_task():
+    print("shutdown")
+    scheduler.shutdown()
+    await db_admin.db.async_close()
 
 
 @app.get("/", response_class=HTMLResponse)
